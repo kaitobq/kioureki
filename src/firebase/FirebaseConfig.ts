5b9194c3503b52b63b255531e9a1b1ace93bee39
kaitobq <kaitobq520@gmail.com>
2024-02-25 15:27:24 +0900

    firebase

---
+++ src/firebase/FirebaseConfig.ts
@@ -0,0 +1,20 @@
+"use client";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+export const FirebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+};
+
+export const firebase = initializeApp(FirebaseConfig);
+
+const auth = getAuth(firebase);
+
+export const FirebaseApp = {
+  auth,
+};
