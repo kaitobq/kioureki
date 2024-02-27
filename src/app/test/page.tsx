ff9e3c9cec268fc100f606980597cb1b5b04de01
kaitobq <kaitobq520@gmail.com>
2024-02-27 22:36:23 +0900

    テスト用ページ

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import InputInjury from "@/components/data/InputDialog";
+import { Box, Toolbar } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box height="100vh">
+      <Toolbar />
+    </Box>
+  );
+}
