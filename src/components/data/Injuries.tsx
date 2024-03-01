8fbde0eb79e7fa73e3fe21105820e73f574db2f6
kaitobq <kaitobq520@gmail.com>
2024-03-01 17:56:50 +0900

    項目追加

--- src/components/data/Injuries.tsx
+++ src/components/data/Injuries.tsx
@@ -14,12 +14,9 @@ import {
 import AddIcon from "@mui/icons-material/Add";
 import React, { useEffect, useRef, useState } from "react";
 import InputDialog from "./InputDialog";
-
-type injury = {
-  name: string;
-  part: string;
-  diagnosis: string;
-};
+import EditIcon from "@mui/icons-material/Edit";
+import EditDialog from "./EditDialog";
+import { injury } from "../types/injury";
 
 const Injuries = () => {
   const { documents: data } = useFirestore("Injuries");
@@ -38,20 +35,28 @@ const Injuries = () => {
     }
   };
 
-  const [digOpen, setDigOpen] = useState(false);
+  const [inputOpen, setInputOpen] = useState(false);
+  const [editOpen, setEditOpen] = useState(false);
+  const [selectedRow, setSelectedRow] = useState<injury | null>(null);
+
+  const handleEditClick = (row: injury) => {
+    setSelectedRow(row);
+    setEditOpen(true);
+    console.log(row);
+  };
 
   return (
-    <Box width="80%">
+    <Box width="80%" my={5}>
       <input type="text" ref={ref} onChange={handleSearch}></input>
       <InputDialog
-        open={digOpen}
+        open={inputOpen}
         onClose={() => {
-          setDigOpen(false);
+          setInputOpen(false);
         }}
       />
       <Button
         onClick={() => {
-          setDigOpen(true);
+          setInputOpen(true);
         }}
       >
         <AddIcon />
@@ -60,22 +65,48 @@ const Injuries = () => {
         <Table>
           <TableHead>
             <TableRow>
+              <TableCell></TableCell>
               <TableCell>name</TableCell>
               <TableCell>part</TableCell>
               <TableCell>diagnosis</TableCell>
+              <TableCell>category</TableCell>
+              <TableCell>note</TableCell>
+              <TableCell>injuryDate</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {searchQuery.map((row) => (
-              <TableRow key={row.name}>
+              <TableRow key={row.id}>
+                <TableCell>
+                  <Button onClick={() => handleEditClick(row)}>
+                    <EditIcon />
+                  </Button>
+                </TableCell>
                 <TableCell>{row.name}</TableCell>
                 <TableCell>{row.part}</TableCell>
                 <TableCell>{row.diagnosis}</TableCell>
+                <TableCell>{row.category}</TableCell>
+                <TableCell>{row.note}</TableCell>
+                <TableCell>
+                  {row.injuryDate
+                    ? row.injuryDate.toDate().toDateString()
+                    : "-"}
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      {selectedRow && (
+        <EditDialog
+          open={editOpen}
+          onClose={() => {
+            setEditOpen(false);
+            setSelectedRow(null);
+          }}
+          initialValues={selectedRow}
+        />
+      )}
     </Box>
   );
 };
