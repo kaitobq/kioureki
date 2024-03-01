1898b8b05ab1a321c08f72f6e1a823e84cd3764d
kaitobq <kaitobq520@gmail.com>
2024-03-01 17:57:05 +0900

    InputDialog作成

--- src/components/data/InputDialog.tsx
+++ src/components/data/InputDialog.tsx
@@ -11,6 +11,10 @@ import {
 import React, { useState } from "react";
 import { FirebaseApp } from "@/firebase/FirebaseConfig";
 import { addDoc, collection } from "firebase/firestore";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs, { Dayjs } from "dayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 const InputDialog = (props: any) => {
   const { open, onClose } = props;
@@ -18,6 +22,9 @@ const InputDialog = (props: any) => {
   const [name, setName] = useState("");
   const [part, setPart] = useState("");
   const [diagnosis, setDiagnosis] = useState("");
+  const [category, setCategory] = useState("");
+  const [note, setNote] = useState("");
+  const [injuryDate, setInjuryDate] = React.useState<Dayjs | null>();
   const [dialogOpen, setDialogOpen] = useState(false);
 
   const handleClose = () => {
@@ -31,60 +38,95 @@ const InputDialog = (props: any) => {
     try {
       const docRef = collection(firestore, "Injuries");
 
-      await addDoc(docRef, { name, part, diagnosis });
+      await addDoc(docRef, {
+        name,
+        part,
+        diagnosis,
+        category,
+        note,
+        injuryDate: injuryDate ? injuryDate.toDate() : null,
+      });
 
       setName("");
       setPart("");
       setDiagnosis("");
+      setCategory("");
+      setNote("");
     } catch (e) {
       console.log(e);
     }
   };
 
   return (
-    <Dialog
-      open={open}
-      onClose={handleClose}
-      PaperProps={{ component: "form" }}
-      onSubmit={handleSubmit}
-    >
-      <DialogTitle>既往歴登録</DialogTitle>
-      <DialogContent>
-        <TextField
-          required
-          id="name"
-          label="name"
-          autoFocus
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          sx={{ marginBottom: 1 }}
-          fullWidth
-        />
-        <TextField
-          required
-          id="part"
-          label="part"
-          value={part}
-          onChange={(e) => setPart(e.target.value)}
-          sx={{ marginBottom: 1 }}
-          fullWidth
-        />
-        <TextField
-          required
-          id="diagnosis"
-          label="diagnosis"
-          value={diagnosis}
-          onChange={(e) => setDiagnosis(e.target.value)}
-          sx={{ marginBottom: 1 }}
-          fullWidth
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button type="submit" variant="contained" fullWidth>
-          Submit
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{ component: "form" }}
+        onSubmit={handleSubmit}
+      >
+        <DialogTitle>既往歴登録</DialogTitle>
+        <DialogContent>
+          <TextField
+            required
+            id="name"
+            label="名前"
+            autoFocus
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            sx={{ marginBottom: 1 }}
+            fullWidth
+          />
+          <TextField
+            required
+            id="part"
+            label="部位"
+            value={part}
+            onChange={(e) => setPart(e.target.value)}
+            sx={{ marginBottom: 1 }}
+            fullWidth
+          />
+          <TextField
+            required
+            id="diagnosis"
+            label="診断"
+            value={diagnosis}
+            onChange={(e) => setDiagnosis(e.target.value)}
+            sx={{ marginBottom: 1 }}
+            fullWidth
+          />
+          <TextField
+            required
+            id="category"
+            label="カテゴリ"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            sx={{ marginBottom: 1 }}
+            fullWidth
+          />
+          <TextField
+            id="note"
+            label="備考"
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+            sx={{ marginBottom: 1 }}
+            fullWidth
+          />
+          <DatePicker
+            label="受傷日"
+            value={injuryDate}
+            onChange={(newValue) => {
+              setInjuryDate(newValue);
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button type="submit" variant="contained" fullWidth>
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </LocalizationProvider>
   );
 };
 
